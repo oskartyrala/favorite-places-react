934aa04f2ccfe60beddad25b451ee70313bfdbab
oskartyrala <oskartyrala@protonmail.com>
2023-06-28 17:04:27 +0200

    Add country for Seville

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App(): JSX.Element {
       <PlaceEntry
       title = "The capital of Andalusia"
       name = "Sevilla"
-      country = "Country placeholder"
+      country = "Spain"
       imageURL = "https://images.pexels.com/photos/5470587/pexels-photo-5470587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       location = "https://goo.gl/maps/8NgWhwFxYtcaZshx8"
       description = "I studied there for 4 months in 2017 and loved it! I've been meaning to go back ever since, but for one reason or another never did."
