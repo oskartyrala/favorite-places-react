b3f3afa5ac3fe44338f2fdf16ff0661c9802ce78
oskartyrala <oskartyrala@protonmail.com>
2023-06-28 16:47:34 +0200

    Add Sevilla to list of places

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,12 @@ function App(): JSX.Element {
       />
 
       <PlaceEntry
-      title = "Title placeholder"
-      name = "Name placeholder"
+      title = "Sevilla"
+      name = "Sevill"
       country = "Country placeholder"
-      imageURL = "#"
-      location = "#"
-      description = "Description placeholder"
+      imageURL = "https://images.pexels.com/photos/5470587/pexels-photo-5470587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      location = "https://goo.gl/maps/8NgWhwFxYtcaZshx8"
+      description = "I studied there for 4 months in 2017 and loved it! I've been meaning to go back ever since, but for one reason or another never did."
       />
       
     </>
