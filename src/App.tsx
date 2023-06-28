8fd06cbf44ed9be5062af2a4bd62a56900e60cc7
oskartyrala <oskartyrala@protonmail.com>
2023-06-28 14:53:12 +0200

    Implement place entries

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import PageHeader from "./PageHeader"
+import PlaceEntry from "./PlaceEntry"
 
 function App(): JSX.Element {
   return (
-    <PageHeader 
-      heading = {"Oskar's Favourite Places"}
-    />
+    <>
+      <PageHeader 
+        heading = {"Oskar's Favourite Places"}
+      />
+
+      <PlaceEntry
+      title = "Title placeholder"
+      name = "Name placeholder"
+      country = "Country placeholder"
+      imageURL = "#"
+      location = "#"
+      description = "Description placeholder"
+      />
+      
+    </>
   )
 }
 
---
+++ src/PlaceEntry.tsx
@@ -0,0 +1,24 @@
+interface Place {
+    title: string;
+    name: string;
+    country: string;
+    imageURL: string;
+    location: string;
+    description: string;
+}
+
+function PlaceEntry(props: Place): JSX.Element {
+    return (
+        <>
+            <h2>{props.title}</h2>
+            <h3>{props.name}</h3>
+            <p>{props.country}</p>
+            <img src={props.imageURL} alt={props.title}></img>
+            <a href={props.location}>Google Maps</a>
+            <p>Why I love it:</p>
+            <p>{props.description}</p>
+        </>
+    )
+}
+
+export default PlaceEntry;
